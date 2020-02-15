a34074ad3f60c56b15b1e7f7399194d8bb799167
Max Carey <maxcarey@protonmail.com>
2020-02-15 14:50:17 -0600

    working for el pais, remove unwanted javascript for el pais, custom trimming of text, get category, and get author

--- scrape_program.py
+++ scrape_program.py
@@ -3,15 +3,36 @@ import jornada_scraper
 import la_prensa_scraper
 import excelsior
 
-#myJornadaScraper = jornada_scraper.jornada()
-#myJornadaScraper.scrape_la_jornada()
-#myJornadaScraper.get_daily_stats()
-#
-#
-#myPrensaScraper = la_prensa_scraper.la_prensa_scraper()
-#myPrensaScraper.scrape_la_prensa()
-#myPrensaScraper.get_daily_stats()
 
-myExcelsiorScrapper = excelsior.excelsior_scraper()
-myExcelsiorScrapper.scrape_excelsior()
-myExcelsiorScrapper.get_daily_stats()
+excelsior_scraper = scrape.daily_scraper("https://www.excelsior.com.mx/rss.xml", 
+										"Excelsior",
+										{"target_tag": "div",
+										"target_tag_att": "id",
+										"target_tag_att_value": "node-article-body"})
+
+excelsior_scraper.get_target_articles_to_scrape()
+excelsior_scraper.scrape_articles()
+excelsior_scraper.get_daily_stats()
+
+
+
+jornada_scraper = scrape.daily_scraper("https://www.jornada.com.mx/rss/edicion.xml?v=1", 
+										"La Jornada",
+										{"target_tag": "div",
+										"target_tag_att": "id",
+										"target_tag_att_value": "article-text"})
+
+jornada_scraper.get_target_articles_to_scrape()
+jornada_scraper.scrape_articles()
+jornada_scraper.get_daily_stats()
+
+
+el_pais_scraper = scrape.daily_scraper("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada",
+									  "El País",
+									  {"target_tag": "div",
+									  "target_tag_att": "id",
+									  "target_tag_att_value": "articulo_contenedor"})
+
+el_pais_scraper.get_target_articles_to_scrape()
+el_pais_scraper.scrape_articles()
+el_pais_scraper.get_daily_stats()
